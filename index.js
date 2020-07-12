4565f9e94347c0ba507bde76dae9479c4a5d973c
Justin Higgins <justin@selfless.io>
2020-07-11 17:13:53 -0700

    remove bg highlight

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const conditions = fs.readFileSync(`./${language}.txt`).toString().split('\n').f
 const starting = `
   \x1b[33mStarting condition:\x1b[0m
 
-  \x1b[42m${conditions[Math.floor(Math.random() * conditions.length - 1)]}\x1b[0m
+  ${conditions[Math.floor(Math.random() * conditions.length - 1)]}
 
 
 `
