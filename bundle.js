35c5f88afd9f4cfe089349405358618fc89d43e2
Justin Higgins <justin@selfless.io>
2021-03-10 19:29:05 -0800

    0.4.7

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports={
     "archive": [
         "Who most recently went to a concert?",
@@ -106,14 +106,19 @@ module.exports={
         "Who most recently took a stretch break?",
         "Who most recently cooked a new food recipe?",
         "Who most recently spoke in another mother tongue?",
-        "Who most recently shouted?"
-    ],
-    "latest": [
+        "Who most recently shouted?",
         "Who most recently inflated a tire?",
         "Who most recently performed an early maintenance?",
         "Who most recently peeled a fruit or vegetable?",
         "Who most recently cooked beans?",
         "Who most recently achieved a stated goal?"
+    ],
+    "latest": [
+        "Who most recently sold something?",
+        "Who most recently travelled a new path?",
+        "Who most recently adopted something?",
+        "Who most recently published a piece of writing?",
+        "Who most recently forgot where they left their keys"
     ]
 }
 
